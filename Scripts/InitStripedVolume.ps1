b95df64658b6b1d8a8a813d610c64df36f99025e
andrewalumkal <andrew.alumkal@gmail.com>
2020-11-19 12:53:57 -0500

    Script to create striped volume

---
+++ Scripts/InitStripedVolume.ps1
@@ -0,0 +1,19 @@
+[cmdletbinding()]
+Param(
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [String]$DriveLetter
+)
+
+$PhysicalDisks = Get-PhysicalDisk | Where-Object {$_.CanPool -eq "True" -and $_.OperationalStatus -eq "OK" -and $_.HealthStatus -eq "Healthy"}
+
+
+New-StoragePool -FriendlyName "DataFiles" -StorageSubsystemFriendlyName "Windows Storage*" `
+    -PhysicalDisks $PhysicalDisks | New-VirtualDisk -FriendlyName "DataFiles" `
+    -Interleave 65536 -NumberOfColumns $PhysicalDisks.Count -ResiliencySettingName simple `
+    –UseMaximumSize |Initialize-Disk -PartitionStyle GPT -PassThru |New-Partition -DriveLetter $DriveLetter `
+    -UseMaximumSize |Format-Volume -FileSystem NTFS -NewFileSystemLabel "DataDisks" `
+    -AllocationUnitSize 65536 -Confirm:$false
+
+New-Item -ItemType Directory -Force -Path "$($DriveLetter):\Data"
+New-Item -ItemType Directory -Force -Path "$($DriveLetter):\Logs"
